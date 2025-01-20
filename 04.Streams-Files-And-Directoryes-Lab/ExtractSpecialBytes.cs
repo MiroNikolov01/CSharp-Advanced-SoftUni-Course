769ed88d3eb758f8cc939d20269910fd46a26a24
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2025-01-20 18:11:28 +0200

    Add files via upload

---
+++ 04.Streams-Files-And-Directoryes-Lab/ExtractSpecialBytes.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace ExtractSpecialBytes
+{
+    using System;
+    using System.IO;
+
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+            HashSet<byte> specialBytes = new HashSet<byte>();
+            using (var speacilBytesReader = new StreamReader(bytesFilePath))
+            {
+                while (!speacilBytesReader.EndOfStream)
+                {
+                    byte specialByteSymbol = byte.Parse(speacilBytesReader.ReadLine());
+                    specialBytes.Add(specialByteSymbol);
+                }
+            }
+
+            using (var inputStream = new FileStream(binaryFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var outputStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                {
+                    byte[] buffer = new byte[1024];
+                    int readBytes;
+                    while ((readBytes = inputStream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        int countSpecialBytes = 0;
+                        for (int i = 0; i < readBytes; i++)
+                        {
+                            if (specialBytes.Contains(buffer[i]))
+                            {
+                                buffer[countSpecialBytes++] = buffer[i];
+                            }
+                        }
+
+                        outputStream.Write(buffer, 0, countSpecialBytes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04.Streams-Files-And-Directoryes-Lab/SplitMergeBinaryFile.cs
@@ -0,0 +1,67 @@
+﻿namespace SplitMergeBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class SplitMergeBinaryFile
+    {
+        static void Main()
+        {
+            string sourceFilePath = @"..\..\..\Files\example.png";
+            string joinedFilePath = @"..\..\..\Files\example-joined.png";
+            string partOnePath = @"..\..\..\Files\part-1.bin";
+            string partTwoPath = @"..\..\..\Files\part-2.bin";
+
+            SplitBinaryFile(sourceFilePath, partOnePath, partTwoPath);
+            MergeBinaryFiles(partOnePath, partTwoPath, joinedFilePath);
+        }
+
+        public static void SplitBinaryFile(string sourceFilePath, string partOneFilePath, string partTwoFilePath)
+        {
+            using (var inputFileReaderStream = new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read))
+            {
+                int firstPartSize = (int)(inputFileReaderStream.Length / 2 + inputFileReaderStream.Length % 2);
+                
+                using (var firstPartStream = new FileStream(partOneFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    CopyExact(inputFileReaderStream, firstPartStream, firstPartSize);
+                }
+
+                int secondPartSize = (int)inputFileReaderStream.Length / 2;
+                
+                using (var secondPartStream = new FileStream(partTwoFilePath, FileMode.Create, FileAccess.Write))
+                {
+                    CopyExact(inputFileReaderStream, secondPartStream,secondPartSize);
+                }
+            }
+        }
+
+        public static void MergeBinaryFiles(string partOneFilePath, string partTwoFilePath, string joinedFilePath)
+        {
+            using (var outputStream = new FileStream(joinedFilePath, FileMode.Create, FileAccess.Write))
+            {
+                using(var firstPartStream = new FileStream(partOneFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    firstPartStream.CopyTo(outputStream);
+                }
+                using (var secondPartStream = new FileStream(partTwoFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    secondPartStream.CopyTo(outputStream);
+                }
+            }
+        }
+
+        private static void CopyExact(FileStream input, FileStream output, int count)
+        {
+            byte[] buffer = new byte[1024];
+            
+            int countReadedBytes = 0;
+            while (countReadedBytes < count)
+            {
+                int readBytes = input.Read(buffer, 0, Math.Min(buffer.Length, count - countReadedBytes));
+                output.Write(buffer, 0, readBytes);
+                countReadedBytes += readBytes;
+            }
+        }
+    }
+}
