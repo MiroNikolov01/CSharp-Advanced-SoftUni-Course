cc86632e0021a88ecd904b556b9d4c22ae2d8605
Stanimir_Nikolov <166949863+MiroNikolov01@users.noreply.github.com>
2025-01-27 01:19:42 +0200

    Add files via upload

---
+++ 04.Streams-Files-And-Directories-Lab/WordCount.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordCount
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            using (StreamReader wordsReader = new StreamReader(wordsFilePath))
+            {
+                //Reading words.txt
+                string wordLine = wordsReader.ReadLine();
+                string[] words = File.ReadAllLines(wordsFilePath)
+                    .SelectMany(line => line.Split(' ',StringSplitOptions.RemoveEmptyEntries))
+                    .ToArray();
+
+                Dictionary<string, int> wordsMap = new Dictionary<string, int>();
+                foreach (var word in words)
+                {
+                    if (!wordsMap.ContainsKey(word))
+                    {
+                        wordsMap.Add(word, 0);
+                    }
+                }
+
+                using (StreamReader textReader = new StreamReader(textFilePath))
+                {
+                    while (!textReader.EndOfStream)
+                    {
+                        string textLine = textReader.ReadLine();
+                        foreach (Match match in Regex.Matches(textLine.ToLower(), @"\w+"))
+                        {
+                            string word = match.Value;
+                            if (wordsMap.ContainsKey(word))
+                            {
+                                wordsMap[word]++;
+                            }
+                        }
+                    }
+                    using (StreamWriter outputWriter = new StreamWriter(outputFilePath))
+                    {
+                        foreach (var word in wordsMap.OrderByDescending(x => x.Value))
+                        {
+                            outputWriter.WriteLine($"{word.Key} - {word.Value}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
